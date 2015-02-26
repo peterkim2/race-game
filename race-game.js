2ffb01dc797f4a357fe879b19b081897b9f3a113
Peter Kim <peterkim23@gmail.com>
2015-02-25 19:04:45 -0800

    added jquery for speed input

--- race-game.js
+++ race-game.js
@@ -13,22 +13,31 @@
     };
   }
 
-  var turtle = new Animal(2, 9, "Mike"), //speed value needed as placeholer. no actual value
+$(document).ready(function() {
+    $('#userInput').keypress(function(event) {
+          if (event.keyCode == 13) {
+            turtle.speed = parseInt($('#mikeSpeed').val());
+            rabbit.speed = parseInt($('#peterSpeed').val());
+            shiba.speed = parseInt($('#roxySpeed').val());
+  }
+  });
+});  
+  var turtle = new Animal(2, 9, "Mike"),
       rabbit = new Animal(11, 4, "Peter"),
       shiba = new Animal(8, 7, "Roxy");
-
+    
   var distance = 40;
-
-  function pickSpeed(object) {
-    object.speed = parseInt(prompt("Pick " + object.name + "'s " + "speed between 1-10")); //parseInt converts input as integer instead of string.
-    while(isNaN(object.speed) || object.speed <= 0 || object.speed > 10) { //isNaN is 'is not a number'
-        object.speed = parseInt(prompt("Please pick " + object.name + "'s " + "speed between 1-10!"));
-    }
-  }
+  
+  //function pickSpeed(object) {
+    //object.speed = parseInt(prompt("Pick " + object.name + "'s " + "speed between 1-10")); //parseInt converts input as integer instead of string.
+    //while(isNaN(object.speed) || object.speed <= 0 || object.speed > 10) { //isNaN is 'is not a number'
+        //object.speed = parseInt(prompt("Please pick " + object.name + "'s " + "speed between 1-10!"));
+    //}
+  //}
   function race() {
-    pickSpeed(turtle); //runs the function pickSpeed
-    pickSpeed(rabbit);
-    pickSpeed(shiba);
+//    pickSpeed(turtle); //runs the function pickSpeed
+//    pickSpeed(rabbit);
+//    pickSpeed(shiba);
       while(turtle.position < distance && rabbit.position < distance && shiba.position < distance) {
       turtle.run();
       rabbit.run();
@@ -50,13 +59,10 @@
     }
 
 }
-//  function replay() {
-//    location.reload(false);
-//  }
+
 var button = document.getElementById("button");
   button.addEventListener("click", race);
+//var userInput = document.getElementById('userInput');
+//  userInput.addEventListener("keyup", race);
 //var replay = document.getElementById("replay");
 //  button.addEventListener("click", replay);
-
-
-
